637c655cff6f8258199e855fd915eeac12a430a7
Runaway4uk <dev.runny@gmail.com>
2021-09-30 13:07:56 +0300

    Добавлен файл .jshintrc

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptive = 6;
 
+console.log(
+  "Привет консолька!",
+  title,
+  screens,
+  screenPrice,
+  rollback,
+  fullPrice
+);
