ccace2fda93e0108b26da49a53db5ef2dfe65763
Max Lebedev <dev.runny@gmail.com>
2021-10-01 11:36:08 +0300

    Урок 2

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const title = 1;
-const screens = 2;
-const screenPrice = 3;
-const rollback = 4;
-const fullPrice = 5;
-const adaptive = 6;
+let str = "Hello my best friend!";
 
-alert("Приветствую Вас!");
-console.log(
-  "Привет консолька!",
-  title,
-  screens,
-  screenPrice,
-  rollback,
-  fullPrice
-);
+console.log(str.split("e"));
