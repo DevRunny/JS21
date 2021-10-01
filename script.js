d21c4b5c27e4e53816eebc650a20e507232a41ae
Max Lebedev <dev.runny@gmail.com>
2021-10-01 16:34:09 +0300

    Пробелы

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let title = "JS21";
-console.log(typeof title);
+
 let screens = "Простые, Сложные, Интерактивные";
-console.log(screens.length);
+
 let screenPrice = 500;
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+
 let rollback = 26;
-console.log(rollback);
+
 let fullPrice = 14990;
 console.log(typeof fullPrice);
 let adaptive = true;
 console.log(typeof adaptive);
+
+
+console.log(typeof title);
+console.log(screens.length);
 
 console.log("Стоимость верстки экранов " + screenPrice + " рублей");
 console.log("Стоимость создания сайта " + fullPrice + " рублей");
