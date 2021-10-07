af586ab378cf74585f5e284158308df167f4c1b9
Max Lebedev <dev.runny@gmail.com>
2021-10-07 19:42:13 +0300

    Завершение обязательного задания

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 "use strict"; //Строгий режим
 
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
 function letsPlay() {
   function play() {
     let doYouWannaPlay = confirm("Ты хочешь сыграть в УГАДАЙ ЧИСЛО?");
 
     if (doYouWannaPlay === true) {
       const сheck = (num) => {
-        function restartGame() {
-          const whatNum = +prompt("Угадай число от 1 до 100");
+        function restartGame(whatNum) {
+          whatNum = prompt("Угадай число от 1 до 100");
+          const isNumber = function (whatNum) {
+            return (
+              !isNaN(parseFloat(whatNum)) &&
+              isFinite(whatNum) &&
+              whatNum !== null
+            );
+          };
 
-          switch (true) {
-            case whatNum > num:
-              alert("Загаданное число меньше");
-              return restartGame();
-            case whatNum < num:
-              alert("Загаданное число больше");
-              return restartGame();
-            case whatNum === num:
-              alert("МОЛОДЕЦ, ПРЯМО В ТОЧКУ!!!");
-              break;
-            case whatNum != Number:
-              alert("Введи число, дурень!");
-              return restartGame();
-
-            default:
-            case whatNum === null:
-              alert("Все, наигрался, спокойной ночи!");
-              break;
+          if (whatNum > num && isNumber(whatNum)) {
+            alert("Загаданное число меньше");
+            restartGame();
+          } else if (whatNum < num && isNumber(whatNum)) {
+            alert("Загаданное число больше");
+            restartGame();
+          } else if (whatNum == num && isNumber(whatNum)) {
+            alert("МОЛОДЕЦ, ПРЯМО В ТОЧКУ!!!");
+          } else if (whatNum === null) {
+            return alert("Все, наигрался, спокойной ночи!");
+          } else if (!isNumber(whatNum)) {
+            alert("Введи число, дурень!");
+            restartGame();
           }
         }
         restartGame();
       };
-      сheck(26);
+      сheck(getRandomInt(1, 100));
     } else if (doYouWannaPlay === false) {
       alert("Ну не хочешь играть, твое право:(");
     }
@@ -40,18 +46,3 @@ function letsPlay() {
 }
 
 letsPlay();
-
-// if (whatNum > num) {
-//   alert("Загаданное число меньше");
-//   restartGame();
-// } else if (whatNum < num) {
-//   alert("Загаданное число больше");
-//   restartGame();
-// } else if (whatNum == num) {
-//   alert("МОЛОДЕЦ, ПРЯМО В ТОЧКУ!!!");
-// } else if (whatNum != Number) {
-//   alert("Введи число, дурень!");
-//   restartGame();
-// } else if (whatNum == null) {
-//   alert("Все, наигрался, спокойной ночи!");
-// }
